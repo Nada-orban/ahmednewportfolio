79322fea3802c7417c003f5e521c0632571310d6
nada <nadasamir9334@gmail.com>
2023-05-09 22:16:53 -0400

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,45 @@
+import '../styles/globals.css'
+import { useEffect } from "react";
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ColorModeContext, useMode } from '../../public/theme'
+import { useRouter } from 'next/router';
+import { styled, useTheme, alpha } from '@mui/material/styles';
+import Drawer1 from '../../Components/Drawer1'
+
+import AOS from "aos";
+
+import "aos/dist/aos.css";
+
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+      delay: 400,
+      duration: 1000,
+    });
+  }, []);
+  const [theme, colorMode] = useMode();
+  const router = useRouter();
+  const { Path } = router;
+
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Drawer1>
+          <Component {...pageProps} />
+          {/* <style jsx global>{`
+              ${theme.palette.mode === 'dark' ? "body{background:#3d3e49} " : "body{background: #fff} "}`
+          }</style> */}
+        </Drawer1>
+
+
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
+  )
+}
