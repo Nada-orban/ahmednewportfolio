74bdc7e2535056fc332c2f8d6d076ca5d2b3b3f8
nada <nadasamir9334@gmail.com>
2023-06-14 11:48:53 -0400

    save with another photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ export default function Home({ photo }) {
         <meta property="og:image" content="https://kvkirthy.github.io/code-samples/assets/dino.png" />
         <meta property="og:url" content="https://kvkirthy.github.io/code-sample/" /> */}
 
-        <meta property="og:image" content={`/public/profile.jpg`} />
+        <meta property="og:image" content={`../../public/profile.jpg`} />
         <link rel="icon" href="/logo.png" />
         <link rel="icon" href="%PUBLIC_URL%/logonada.png" />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
